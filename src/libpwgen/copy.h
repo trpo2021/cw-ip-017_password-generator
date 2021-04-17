01cdee006734dbfc49014ffce41a67fe7ca63952
Ivan Izvekov <ivan.izvekov02@mail.ru>
2021-04-17 09:36:21 +0700

    Add header files

---
+++ src/libpwgen/copy.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <windows.h>
+
+using namespace std;
+
+int copy(string& Pass);
---
+++ src/libpwgen/reg.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int Reg(int n, string& Pass, int Mask[], int k);
