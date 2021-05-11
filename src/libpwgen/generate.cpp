7b0b5e1f7599768d3cc50cc5abe3fc6287f659f2
Sofya Mogutova <smogutova1234@gmail.com>
2021-05-11 09:58:24 +0700

    Fix code num/generate

--- src/libpwgen/generate.cpp
+++ src/libpwgen/generate.cpp
@@ -8,12 +8,12 @@ using namespace std;
 
 int Generate(int n, string& Pass, int Mask[])
 {
-	int* A = new int[n];
-	for (int i = 0; i < n; i++) {
-		A[i] = i;
-		Pass.append(1, rand() % 26 + 'a');
-		Mask[A[i]] = 0;
-	}
-	delete[] A;
-	return 0;
+    int* A = new int[n];
+    for (int i = 0; i < n; i++) {
+        A[i] = i;
+        Pass.append(1, rand() % 26 + 'a');
+        Mask[A[i]] = 0;
+    }
+    delete[] A;
+    return 0;
 }
--- src/libpwgen/num.cpp
+++ src/libpwgen/num.cpp
@@ -4,16 +4,16 @@
 using namespace std;
 int Num(int n, string& Pass, int Mask[], int k)
 {
-	int temp;
-	int* A = new int[k];
-	for (int i = 0; i < k; i++) {
-		temp = rand() % n;
-		A[i] = temp;
-	}
-	for (int i = 0; i < k; i++) {
-		Pass[A[i]] = rand() % 10 + '0';
-		Mask[A[i]] = 1;
-	}
-	delete[] A;
-	return 0;
+    int temp;
+    int* A = new int[k];
+    for (int i = 0; i < k; i++) {
+        temp = rand() % n;
+        A[i] = temp;
+    }
+    for (int i = 0; i < k; i++) {
+        Pass[A[i]] = rand() % 10 + '0';
+        Mask[A[i]] = 1;
+    }
+    delete[] A;
+    return 0;
 }
