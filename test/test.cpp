2801a4a297a7dd047c5a12402f69bfa273e80a9a
panda <panda@DESKTOP-O22JSD8>
2021-05-31 01:13:17 +0700

    добавил тесты к функциям Odn SmZn и Vibor

--- test/test.cpp
+++ test/test.cpp
@@ -91,3 +91,49 @@ TEST_CASE("num", " ")
     }
     CHECK(k == 2);
 }
+TEST_CASE("SmZn", " ")
+{
+    string Pass = "z";
+    SmZn(10, '0', Pass, 0);
+    CHECK(((Pass[0] != '0') && (Pass[0] != '1') && (Pass[0] != '2')
+           && (Pass[0] != '5'))
+          == true);
+    SmZn(26, 'A', Pass, 0);
+    CHECK(((Pass[0] != 'I') && (Pass[0] != 'S') && (Pass[0] != 'Z')
+           && (Pass[0] != 'O'))
+          == true);
+    SmZn(26, 'a', Pass, 0);
+    CHECK(((Pass[0] != 'o') && (Pass[0] != 's') && (Pass[0] != 'z')
+           && (Pass[0] != 'l'))
+          == true);
+}
+TEST_CASE("Vibor", " ")
+{
+    string Pass = "z";
+    int Mask[1] = {0};
+    Vibor(0, 10, Pass, Mask);
+    CHECK(islower(Pass[0]) != 0);
+    Pass[0] = 'Z';
+    Mask[0] = 2;
+    Vibor(0, 9, Pass, Mask);
+    CHECK(isupper(Pass[0]) != 0);
+    Pass[0] = '1';
+    Mask[0] = 1;
+    Vibor(0, 5, Pass, Mask);
+    CHECK(((Pass[0] >= '0') && (Pass[0] <= '9')) == true);
+}
+TEST_CASE("Odn", " ")
+{
+    string Pass = "o1Z";
+    int Mask[3] = {0, 1, 2};
+    Odn(3, Pass, Mask);
+    CHECK(((Pass[0] != 'o') && (Pass[0] != 's') && (Pass[0] != 'z')
+           && (Pass[0] != 'l'))
+          == true);
+    CHECK(((Pass[1] != '0') && (Pass[1] != '1') && (Pass[1] != '2')
+           && (Pass[1] != '5'))
+          == true);
+    CHECK(((Pass[2] != 'I') && (Pass[2] != 'S') && (Pass[2] != 'Z')
+           && (Pass[2] != 'O'))
+          == true);
+}
